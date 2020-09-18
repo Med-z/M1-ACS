832f0be5da5b77b03fd3726b8b25ea9a066c3a90
antoine-white <antoine.maxime.bl@gmail.com>
2020-09-18 16:46:00 +0200

    copy canevas base code >code given by teacher

---
+++ TP1/EXO1/Protocol.java
@@ -0,0 +1,13 @@
+
+/**
+ * Public class to support all the protocol's parameters
+ * @author pmeseure
+ */
+public class Protocol
+{
+  /** Server Port */
+  static public final int PORT=1234;
+
+  // Include here any other value that must be shared between server and clients
+};
+
