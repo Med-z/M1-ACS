f7cf4f47335f8c4b1bb646b987ef5e379b1ee604
antoine-white <antoine.maxime.bl@gmail.com>
2020-09-19 10:14:38 +0200

    add more error codes >also add comment and indentation

--- TP1/1.2/Protocol.java
+++ TP1/1.2/Protocol.java
@@ -10,6 +10,15 @@ public class Protocol {
 
     // Include here any other value that must be shared between server and clients
 
+    /************************
+           STATUS CODES
+    *************************/
+
+    //OK STATUS
     static public final int OK = 200;
+    //CLIENT ERROR
     static public final int ERROR_LANG = 400;
+    static public final int ERROR_ZONE = 401;
+    //SERVER ERROR
+    static public final int ERROR_SERVER = 500;
 };
